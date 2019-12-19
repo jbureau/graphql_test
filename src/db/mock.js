96d98cd053bfc8ab6d04be087b0bbb3e63b89e35
Julie Bureau <bureau.julie@ymail.com>
2019-12-19 16:00:51 +0100

    Init Db mock

--- src/db/mock.js
+++ src/db/mock.js
@@ -1,2 +1,46 @@
-const planets = require('./planets.json')
-const spaceCenters = require('./space-centers.json')
+const planetsList = require('./planets.json') || []
+const spaceCentersList = require('./space-centers.json') || []
+
+const planets = planetsList.reduce((map, planet) => {
+  if (planet) map[planet.code] = planet
+  return map
+}, {})
+
+const spaceCenters = spaceCentersList.reduce((map, spaceCenter) => {
+  if (spaceCenter) map[spaceCenter.uid] = spaceCenter
+  return map
+}, {})
+
+const spaceCenterIdsByPlanetCode = spaceCentersList.reduce((map, spaceCenter) => {
+  if (spaceCenter) {
+    if (!map[spaceCenter.planet_code]) map[spaceCenter.planet_code] = []
+    map[spaceCenter.planet_code].push(spaceCenter.uid)
+  }
+  return map
+}, {})
+
+/**
+ * Database mock
+ * Does not store any data
+ */
+const DBMock = {
+  planets: {
+    findOneByCode: function(code) {
+      return planets[code]
+    }
+  },
+  spaceCenters: {
+    findOne: function(uid) {
+      return spaceCenters[uid]
+    },
+    findAllByPlanetCode: function(planetCode) {
+      const spaceCentersForPlanetCode = spaceCenterIdsByPlanetCode[planetCode]
+      return spaceCentersForPlanetCode.reduce((list, id) => {
+        if (id && spaceCenters[id]) list.push(spaceCenters[id])
+        return list
+      }, [])
+    }
+  }
+}
+
+module.exports = DBMock
